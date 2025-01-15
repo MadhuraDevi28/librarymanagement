f4d8ab287c7de5bdd5dcc22d4c27a5d7bfc6cc89
madhu2628 <maduradevi3110@gmail.com>
2025-01-16 00:08:12 +0530

    validation for login

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule, EmailValidator } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  standalone:true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss'
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit
 
-}
+{
+  userForm!: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+  ){}
+  ngOnInit(): void {this.initialiseForm();
+    
+  }
+  initialiseForm(): void {
+    this.userForm = this.fb.group({
+      emailId: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      role:['',[Validators.required]]
+    }); 
+  }
+  
+  loginUser(): void {console.log(this.userForm.value)
+  }
+} 
+
